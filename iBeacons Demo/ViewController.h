e6d5897004e8f6e4f9e68e58bb1028d7064cb660
M Newill <matt@gvenue.com>
2013-09-27 08:13:25 +0100

    Initial Commit

---
+++ iBeacons Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeacons Demo
+//
+//  Created by M Newill on 27/09/2013.
+//  Copyright (c) 2013 Mobient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
